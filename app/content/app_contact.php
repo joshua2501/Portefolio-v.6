4cd2ea5b4d51671254461ad9a8862734a53c4a23
joshua2501 <joshua2501@live.fr>
2014-12-16 21:57:24 +0100

    divers

--- app/content/app_contact.php
+++ app/content/app_contact.php
@@ -4,10 +4,9 @@
         <p>
             Si vous avez des questions concernant une des partie de ce site ou si vous avez des questions pour un de vos projet ou d'autres sites que j'ai développer, merci de remplir le formulaire ci-dessous.
         </p>
-
         <!-- Do not change the code! -->
         <a id="foxyform_embed_link_151961" href="http://fr.foxyform.com/">foxyform</a>
-        <script type="text/javascript" style="margin: 0 auto;">
+        <script type="text/javascript">
             (function (d, t) {
                 var g = d.createElement(t),
                     s = d.getElementsByTagName(t)[0];
@@ -16,43 +15,5 @@
             }(document, "script"));
         </script>
         <!-- Do not change the code! -->
-
-        <!--
-        <form method="post" action="traitement_formulaire.php" class="contact">
-            <fieldset>
-                <legend>Vos coordonnées</legend>
-                <p>
-                    <label for="nom">Nom* :</label>
-                    <input type="text" id="nom" name="nom" tabindex="1" autofocus required/>
-                </p>
-
-                <p>
-                    <label for="email">Email* :</label>
-                    <input type="text" id="email" name="email" tabindex="2" required/>
-                </p>
-            </fieldset>
-
-            <fieldset>
-                <legend>Votre message :</legend>
-                <p>
-                    <label for="objet">Objet* :</label>
-                    <input type="text" id="objet" name="objet" tabindex="3" required/>
-                </p>
-
-                <p>
-                    <label for="message">Message* :
-                        <br />
-                    </label>
-                    <textarea id="message" name="message" tabindex="4" cols="40" rows="8" required></textarea>
-                </p>
-            </fieldset>
-
-            <small>* Champs obligatoire</small>
-            <div style="text-align:center;">
-                <input type="submit" name="envoi" value="Envoyer le formulaire !" />
-            </div>
-        </form>
--->
-
     </article>
 </section>
