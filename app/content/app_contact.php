da1e99e644fced12de4691f115e63fca1c4a66bb
joshua2501 <joshua2501@live.fr>
2014-12-16 22:49:07 +0100

    changemenr def

--- app/content/app_contact.php
+++ app/content/app_contact.php
@@ -4,16 +4,7 @@
         <p>
             Si vous avez des questions concernant une des partie de ce site ou si vous avez des questions pour un de vos projet ou d'autres sites que j'ai développer, merci de remplir le formulaire ci-dessous.
         </p>
-        <!-- Do not change the code! -->
-        <a id="foxyform_embed_link_151961" href="http://fr.foxyform.com/">foxyform</a>
-        <script type="text/javascript">
-            (function (d, t) {
-                var g = d.createElement(t),
-                    s = d.getElementsByTagName(t)[0];
-                g.src = "http://fr.foxyform.com/js.php?id=151961&sec_hash=d6c3c0ffa2e&width=350px";
-                s.parentNode.insertBefore(g, s);
-            }(document, "script"));
-        </script>
-        <!-- Do not change the code! -->
+        <iframe src="http://fr.foxyform.com/form.php?id=151961&amp;sec_hash=d6c3c0ffa2e" width="350px" height="446px" style="border: none;margin-left: 280px;" frameborder="0" kwframeid="1"></iframe>
     </article>
 </section>
+
