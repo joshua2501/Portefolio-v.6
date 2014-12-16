310db5e163b34c02791f34e265074303119577de
joshua2501 <joshua2501@live.fr>
2014-12-16 21:35:48 +0100

    remplacement du form contact pour test

--- app/content/app_contact.php
+++ app/content/app_contact.php
@@ -4,6 +4,20 @@
         <p>
             Si vous avez des questions concernant une des partie de ce site ou si vous avez des questions pour un de vos projet ou d'autres sites que j'ai développer, merci de remplir le formulaire ci-dessous.
         </p>
+
+<!-- Do not change the code! -->
+<a id="foxyform_embed_link_151960" href="http://fr.foxyform.com/">foxyform</a>
+<script type="text/javascript">
+(function(d, t){
+   var g = d.createElement(t),
+       s = d.getElementsByTagName(t)[0];
+   g.src = "http://fr.foxyform.com/js.php?id=151960&sec_hash=3da3dbafacd&width=350px";
+   s.parentNode.insertBefore(g, s);
+}(document, "script"));
+</script>
+<!-- Do not change the code! -->
+
+<!--
         <form method="post" action="traitement_formulaire.php" class="contact">
             <fieldset>
                 <legend>Vos coordonnées</legend>
@@ -38,5 +52,7 @@
                 <input type="submit" name="envoi" value="Envoyer le formulaire !" />
             </div>
         </form>
+-->
+
     </article>
 </section>
