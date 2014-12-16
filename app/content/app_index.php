7876f99eef7d03ffb6cba361c8ebe87b882a8c15
joshua2501 <joshua2501@live.fr>
2014-12-16 01:08:38 +0100

    test 0 - enlever point d'interogation

---
+++ app/content/app_index.php
@@ -0,0 +1,31 @@
+<section>
+    <article>
+        <h2>Qui suis-je</h2>
+        <img src="src/img/contenu/portrait.png" alt="renaud de mascarel" class="imgleft" class="portrait" />
+        <p>
+            Je me nomme Renaud de MASCAREL et je suis magasinier chez VDM, a Paris.
+        </p>
+        <p>
+            Vous trouverez sur ce site, que j'ai développer, toute mes informations professionnel en tant que <a href="magasinier.php">Magasinier</a> et <a href="developpeur.php">Développeur Front-end</a> en autodidacte.
+        </p>
+        <p>
+            Je peux vous donner des conseils sur les <a href="entretien.php">entretiens d'embauche</a> et aussi un listing <a href="questionentretien.php">des questions les plus souvent posées</a>.
+        </p>
+        <p>
+            Vous pouvez télécharger mon curriculum vitae au format
+            <a href="src/upload/cvodt.odt">
+                <img alt="mon cv en fichier odt" src="src/img/contenu/cvodt.png" title="Mon CV en format .ODT" />
+            </a>,
+            <a href="src/upload/cvpdf.pdf">
+                <img alt="mon cv en fichier pdf" src="src/img/contenu/cvpdf.png" title="Mon CV en format .PDF" />
+            </a>et
+            <a href="src/upload/cvrtf.rtf">
+                <img alt="mon cv en fichier rtf" src="src/img/contenu/cvrtf.png" title="Mon CV en format .RTF" />
+            </a>
+        </p>
+        <p>
+            Bonne Visite !
+        </p>
+        <span class="clear"></span>
+    </article>
+</section>
