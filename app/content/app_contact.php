0651e1585e7f65bcbc7ea73db3f005c269e2a75e
joshua2501 <joshua2501@live.fr>
2014-12-16 21:44:01 +0100

    modif form contact

--- app/content/app_contact.php
+++ app/content/app_contact.php
@@ -5,19 +5,19 @@
             Si vous avez des questions concernant une des partie de ce site ou si vous avez des questions pour un de vos projet ou d'autres sites que j'ai développer, merci de remplir le formulaire ci-dessous.
         </p>
 
-<!-- Do not change the code! -->
-<a id="foxyform_embed_link_151960" href="http://fr.foxyform.com/">foxyform</a>
-<script type="text/javascript">
-(function(d, t){
-   var g = d.createElement(t),
-       s = d.getElementsByTagName(t)[0];
-   g.src = "http://fr.foxyform.com/js.php?id=151960&sec_hash=3da3dbafacd&width=350px";
-   s.parentNode.insertBefore(g, s);
-}(document, "script"));
-</script>
-<!-- Do not change the code! -->
+        <!-- Do not change the code! -->
+        <a id="foxyform_embed_link_151961" href="http://fr.foxyform.com/">foxyform</a>
+        <script type="text/javascript" class="contact">
+            (function (d, t) {
+                var g = d.createElement(t),
+                    s = d.getElementsByTagName(t)[0];
+                g.src = "http://fr.foxyform.com/js.php?id=151961&sec_hash=d6c3c0ffa2e&width=350px";
+                s.parentNode.insertBefore(g, s);
+            }(document, "script"));
+        </script>
+        <!-- Do not change the code! -->
 
-<!--
+        <!--
         <form method="post" action="traitement_formulaire.php" class="contact">
             <fieldset>
                 <legend>Vos coordonnées</legend>
