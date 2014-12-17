19f9434f0ad82a90906d1ec1c2923517c497c484
joshua2501 <joshua2501@live.fr>
2014-12-18 00:47:31 +0100

    rajout class "center"

--- app/content/app_mentionslegals.php
+++ app/content/app_mentionslegals.php
@@ -1,8 +1,8 @@
 <section>
     <article>
         <h2>Les mentions légals</h2>
-        <p>Le présent <a href="http://www.renauddemascarel.com">site</a> est la propriété personnel de Renaud de mascarel développeur Front-end.</p>
-        <p>
+        <p class="center">Le présent <a href="http://www.renauddemascarel.com">site</a> est la propriété de Renaud de mascarel (développeur Front-end).</p>
+        <p class="center">
             Le prestataire assurant l'hébergement du site est la société <a href="http://www.olympe.in">OLYMPE</a>
             dont le siége social se trouve au :<br />
             31 avenue Sainte Victoire.<br />
@@ -11,8 +11,7 @@
             N° de l'association : 20070028.
         </p>
 
-        <br />
-        <p>
+        <p class="center">
             Le registrar est <a href="http://www.amen.fr ">AMEN SAS.</a><br />
             12-14, Rond Point des Champs Elysées.<br />
             75008 Paris.<br />
